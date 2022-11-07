a7da32c388116a6f799f7eda3019e26ca2d4ec7c
EbukaNweje <nwejeebuka@gmail.com>
2022-11-07 18:38:45 +0100

    new black

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   // const Nav = useNavigate()
   return (
-    <div className='HomeContainer'>
-        <div className='H1'><h1>Get Verified on Instagram</h1></div>
+    <div className='HomeContainerText'>
+        {/* <div className='H1'><h1>Get Verified on Instagram</h1></div>
         <p>
         Let us help you get verified on Instagram. No bots or gimmicks, a 100% human-powered service to help you get a blue tick.
         </p>
        <Link to="#Pricing">
           <div className='Button'>GET STARTED TODAY <AiOutlineArrowDown className='BtnT'/></div>
-       </Link>
+       </Link> */}
     </div>
   )
 }
